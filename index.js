7e0c8d2034d83b2995284932cc862bf7889f0372
Brian Kirkpatrick <code@tythos.net>
2022-12-14 20:02:59 -0800

    removed old header and updated index with metadata

--- index.js
+++ index.js
@@ -3,14 +3,24 @@
  */
 
 import TrippyButton from "./TrippyButton/index.js";
-import Header from "./Header/index.js";
+import TrippyHeader from "./TrippyHeader/index.js";
 
 window.addEventListener("load", () => {
-    let h = new Header()
+    let h = new TrippyHeader()
         .set("title", "Some Stuff")
         .append();
     let b = new TrippyButton()
         .set("label", "My Button")
         .on("click", console.log)
         .append();
+});
+
+export default Object.assign(import.meta, {
+    "__url__": "https://github.com/Tythos/trippy-testdemo.git",
+    "__semver__": "v0.1.0",
+    "__license__": "MIT",
+    "__deps__": {
+        "TrippyButton": "https://github.com/Tythos/TrippyButton.git",
+        "TripperHeader": "https://github.com/Tythos/TrippyHeader.git"
+    }
 });
