ce3c79a45a4952bd5946fff92d2104d873abca6e
Brian Kirkpatrick <code@tythos.net>
2022-12-14 19:57:30 -0800

    button is now submodule reference

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
  * @author <code@tythos.net>
  */
 
-import Button from "./Button/index.js";
+import TrippyButton from "./TrippyButton/index.js";
 import Header from "./Header/index.js";
 
 window.addEventListener("load", () => {
     let h = new Header()
         .set("title", "Some Stuff")
         .append();
-    let b = new Button()
+    let b = new TrippyButton()
         .set("label", "My Button")
         .on("click", console.log)
         .append();
